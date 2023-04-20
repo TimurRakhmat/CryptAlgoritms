a46e20880ccf145c5e009a7bcf98c9bbe9b7fb32
Timur Rakhmatullin <t.rakhmatullin@rusbitech.ru>
2023-04-20 18:18:25 +0300

    galua

---
+++ Rijndael/GaloisField.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rijndael
+{
+    internal class GaloisField
+    {
+        public GaloisField() { }
+
+        static public byte add(byte left, byte right)
+        {
+            return (byte)(left ^ right);
+        }
+
+        static public byte mul(byte a, byte b, byte mod = 0x1b)
+        {
+            byte p = 0;
+
+            while(a != 0 && b != 0)
+            {
+                if ((b & 1) != 0)
+                {
+                    p ^= a;
+                }
+
+                if ((a & 0x80) != 0)
+                    a = (byte)((a << 1) ^ mod);
+                else
+                    a <<= 1;
+
+                b >>= 1;
+            }
+
+            return p;
+        }
+
+
+        static public byte pow(byte a, int n, byte mod = 0x1b)
+        {
+            if (n == 0)
+                return 1;
+            else if (n % 2 == 0)
+            {
+                return pow(mul(a, a, mod), n / 2, mod);
+            }
+            else
+            {
+                byte sq = mul(a, a, mod);
+                return mul(sq, pow(a, n / 2, mod), mod);
+            }
+        }
+
+        static public byte inverse(byte a, byte mod = 0x1b)
+        {
+            return pow(a, 254, mod);
+        }
+    }
+}
--- Rijndael/Program.cs
+++ Rijndael/Program.cs
@@ -1,5 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Des;
+using Rijndael;
+using System.ComponentModel;
+
 Console.WriteLine("Hello, World!");
 
-CryptCore cryptCore = new CryptCore();
+
+byte a = 0x57;
+byte b = 0x83;
+byte c = 0x1b;
+
+byte r = GaloisField.mul(a, b, c);
+
+byte bb = GaloisField.inverse(b);
+
+r = GaloisField.mul(b, bb);
+
+Console.WriteLine(r);
